1490ef47748de1e1d3f9225ca3bc3b93799690b7
Roman Kotov <oxide1994@gmail.com>
2016-11-09 12:27:32 +0300

    syntax fix

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var url = require("url");
 const hostname = '127.0.0.1';
 const port = 8000;
 
-function format_date(date){
-  return date < 10 ? ("0" + date) : date;
+function format_date(date) {
+  return date < 10 ? "0" + date : date;
 }
 
 if (cluster.isMaster) {
@@ -17,51 +17,52 @@ if (cluster.isMaster) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
+  cluster.on('exit', (worker) => {
     console.log(`worker ${worker.process.pid} died`);
   });
 } else {
-  server = http.createServer((req, res) => {
+  var server = http.createServer((req, res) => {
     var method = req.method;
     var ip = req.connection.remoteAddress;
-    var url_info  = url.parse(req.url)
+    var url_info  = url.parse(req.url);
     var options = {
       hostname: url_info.hostname,
       port: url_info.port,
       method: method,
       path: url_info.path,
       headers: req.headers
-    }
-    var proxy_req = http.request(options)
+    };
+    var proxy_req = http.request(options);
     proxy_req.on('response', function(proxy_res) {
       proxy_res.on('data', function(chunk) {
-        res.write(chunk, 'binary')
-      })
+        res.write(chunk, 'binary');
+      });
       proxy_res.on('end', function() {
-        res.end()
-      })
-      res.writeHead(proxy_res.statusCode, proxy_res.headers)
-    })
+        res.end();
+      });
+      res.writeHead(proxy_res.statusCode, proxy_res.headers);
+    });
     req.on('data', function(chunk) {
-      proxy_req.write(chunk, 'binary')
-    })
+      proxy_req.write(chunk, 'binary');
+    });
     req.on('end', function() {
-      proxy_req.end()
-    })
+      proxy_req.end();
+    });
     // формируем время и дату
-    var date = new Date;
-    var curren_date = date.getFullYear() + '-'
-      + format_date(date.getMonth() + 1) + '-'
-      + format_date(date.getDate());
-    var current_time = format_date(date.getHours()) + ':'
-      + format_date(date.getMinutes()) + ':'
-      + format_date(date.getSeconds());
+    var date = new Date();
+    var curren_date = date.getFullYear() + '-' +
+       format_date(date.getMonth() + 1) + '-' +
+       format_date(date.getDate());
+    var current_time = format_date(date.getHours()) + ':' +
+       format_date(date.getMinutes()) + ':' +
+       format_date(date.getSeconds());
     // собираем все данные в одну строку
-    var data = '[' + curren_date + ' ' + current_time + ']' + ' ' + ip + ' ' + method + ' ' +  req.url;
-    fs.appendFile('log.txt', data + '\n', function(err){
-      if (err){
+    var data = '[' + curren_date + ' ' + current_time + '] ' + ip +
+      ' ' + method + ' ' + req.url;
+    fs.appendFile('log.txt', data + '\n', function(err) {
+      if (err) {
         return console.log(err);
-      }else{
+      } else {
         console.log(`${data} saved!`);
       }
     });
